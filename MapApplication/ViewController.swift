ccc0c5e0e6cc0b5651ca18350f6461aa25089b0e
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-18 12:22:50 +0530

    showing markers for all cities from data in json file

--- MapApplication/ViewController.swift
+++ MapApplication/ViewController.swift
@@ -22,39 +22,39 @@ class ViewController: UIViewController {
     }*/
     
     override func loadView() {
+        var json: [String : AnyObject] = [:]
         
         //loading json data
-        if let path = Bundle.main.path(forResource: "places", ofType: "json")
-        {
-            // Load the contents of the file into an NSData object
-            if let jsonData = NSData(contentsOfFile: path)
-            {
-                
-                do {
+        let path = Bundle.main.path(forResource: "places", ofType: "json")
+        
+        // Load the contents of the file into an NSData object
+        let jsonData = NSData(contentsOfFile: path!)
+        do {
                     
-                    // Serialize the jsonData object to make a Json object
-                    let jsonResult = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.allowFragments)
+                // Serialize the jsonData object to make a Json object
+                let jsonResult = try JSONSerialization.jsonObject(with: jsonData! as Data, options: JSONSerialization.ReadingOptions.allowFragments)
                     
-                    let json = jsonResult as? [String : AnyObject]
+                json = (jsonResult as? [String : AnyObject])!
 
                 } catch {
                     print("Problem converting jsonResult to dictionary")
-                }
             }
-        }
-    
         
         // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        // california map
+        let camera = GMSCameraPosition.camera(withLatitude: 36.77, longitude: -119.41, zoom: 5.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
         // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.map = mapView
+        
+        for city in json["cities"] as! [AnyObject] {
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: city["latitude"] as! Double, longitude: city["longitude"] as! Double)
+            marker.title = city["title"] as? String
+            marker.map = mapView
+        }
+        
         
     }
         
